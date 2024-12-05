213085398f3d75e77b7920bea3718adb97ea5155
Muhammad Masyhuda <85928499+huda-7077@users.noreply.github.com>
2024-12-05 12:50:13 +0700

    feat: initial setup (#1)

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,12 @@
+import ExplorePage from "@/features/explore/ExplorePage";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <ExplorePage />
+    </>
+  );
+};
+
+export default page;
---
+++ src/features/home/index.tsx
@@ -0,0 +1,13 @@
+import EventsSection from "./EventsSection";
+import HeroSection from "./HeroSection";
+
+const HomePage = () => {
+  return (
+    <main className="">
+      <HeroSection />
+      <EventsSection />
+    </main>
+  );
+};
+
+export default HomePage;
