c70b7f26702b7f2f492bec060ac78268d838cfac
huda-7077 <85928499+huda-7077@users.noreply.github.com>
2024-12-12 17:18:08 +0700

    feat: add login & register

--- src/features/forgot-password/index.tsx
+++ src/features/forgot-password/index.tsx
@@ -3,28 +3,27 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import useForgotPassword from "@/hooks/api/auth/useForgotPassword";
 import { useFormik } from "formik";
-import { LoginSchema } from "./schemas";
-import useLogin from "@/hooks/api/auth/useLogin";
-import Link from "next/link";
+import { ForgotPasswordSchema } from "./schemas";
+const ForgotPasswordPage = () => {
+  const { mutateAsync: forgotPassword, isPending } = useForgotPassword();
 
-const LoginPage = () => {
-  const { mutateAsync: login, isPending } = useLogin();
   const formik = useFormik({
     initialValues: {
       email: "",
-      password: "",
     },
-    validationSchema: LoginSchema,
+    validationSchema: ForgotPasswordSchema,
+
     onSubmit: async (values) => {
-      await login(values);
+      await forgotPassword(values);
     },
   });
   return (
     <main className="flex justify-center pt-20">
       <Card className="w-[350px]">
         <CardHeader>
-          <CardTitle>Login</CardTitle>
+          <CardTitle>Forgot Password</CardTitle>
         </CardHeader>
         <CardContent>
           <form onSubmit={formik.handleSubmit}>
@@ -43,46 +42,19 @@ const LoginPage = () => {
                   <p className="text-xs text-red-500">{formik.errors.email}</p>
                 ) : null}
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="password">Password</Label>
-                <Input
-                  name="password"
-                  type="password"
-                  placeholder="Password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {!!formik.touched.password && !!formik.errors.password ? (
-                  <p className="text-xs text-red-500">
-                    {formik.errors.password}
-                  </p>
-                ) : null}
-              </div>
             </div>
             <Button
               type="submit"
               className="mt-4 w-full bg-[#80AE44] text-black hover:bg-[#9AC265]"
               disabled={isPending}
             >
-              {isPending ? "loading..." : "Login"}
+              {isPending ? "loading..." : "Submit"}
             </Button>
           </form>
-          <div className="mt-4 text-center text-sm">
-            Dont have an account ?{" "}
-            <Link href="/register" className="text-blue-500 underline">
-              Register
-            </Link>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            <Link href="/forgot-password" className="text-blue-500 underline">
-              Forgot Password
-            </Link>
-          </div>
         </CardContent>
       </Card>
     </main>
   );
 };
 
-export default LoginPage;
+export default ForgotPasswordPage;
